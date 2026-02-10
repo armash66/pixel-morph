1c0f1c0e20334d424ea64d251ad37b56831afc2b
Ansari Armash <ansariarmash007@gmail.com>
2026-02-11 01:47:00 +0530

    Refactor: Add update_info.js and refine layout spacing

---
+++ public/js/update_info.js
@@ -0,0 +1,8 @@
+export function updateInfo() {
+    console.log("Info update script loaded");
+    // Future logic to update info page content dynamically
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    updateInfo();
+});
